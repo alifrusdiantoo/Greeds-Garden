3dada6675f529ef1e7e8cddc58578fe8cf99cc80
Alif Rusdianto <alifrusdianto30@gmail.com>
2025-12-01 23:22:45 +0700

    feat(character): add sfx for cow and chicken
    Add BGM in game and SFX for cow and chicken NPC

---
+++ scenes/components/audio_play_time_component.gd
@@ -0,0 +1,11 @@
+extends Timer
+
+@export var audio_stream_player_2d: AudioStreamPlayer2D
+
+func _ready() -> void:
+	if not is_connected("timeout", Callable(self, "_on_timeout")):
+		connect("timeout", Callable(self, "_on_timeout"))
+
+func _on_timeout() -> void:
+	audio_stream_player_2d.play()
+	print("SFX played")
